1f1ad27cb2dea4cf96978a2af176bac109e904e3
Botir, Bahodirov <benjamin10882579@yahoo.com>
2019-11-21 02:05:07 -0800

    Reply with inlineKeyboard on /start

--- bot.js
+++ bot.js
@@ -1,17 +1,31 @@
 const Telegraf  = require('telegraf');
+const Markup    = require('telegraf/markup');
+const fs        = require('fs');
 
 const bot       = new Telegraf(process.env.TOKEN);
 const private   = process.env.PRIVATE_GROUP;
 
+const inlineKeyboard = Markup.inlineKeyboard([
+  Markup.callbackButton('CV Resume', 'download_resume'),
+  Markup.urlButton("GitHub", "www.github.com/10882579")
+]).extra()
+
 bot.start( (chat) => {
   if(chat.message.chat.id != private){
-    chat.reply("Hi,\nMy name is Botir. Please leave your message here and I will respond within 2 days.\nThanks!");
+    chat.reply("Hi,\nMy name is Botir. Please leave your message here and I will respond within 2 days.\nThanks!", inlineKeyboard);
   }
 })
 
 bot.on('text', ({ message, telegram }) => {
   const { first_name, username } = message.from;
   telegram.sendMessage(private, `${first_name} | @${username}:\n\n${message.text}`);
+})
+
+bot.action('download_resume', (chat) => {
+  chat.replyWithDocument({
+    filename: 'resume.pdf', 
+    source: fs.readFileSync('./assets/resume.pdf')
+  })
 })
 
 module.exports = bot;
